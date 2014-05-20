89b68f81982ae223fcd711456915158fb3b4cc2d
Tyler Perkins <tyler_perkins@me.com>
2014-05-20 16:30:18 -0600

    Modified to employ a closure to capture the total and previous value instead of a pair. Nice!

--- problem_092.clj
+++ problem_092.clj
@@ -8,14 +8,20 @@
 
 (defn solution [roman-str]
   (let [valForChar {\z 0, \I 1, \V 5, \X 10, \L 50, \C 100, \D 500, \M 1000}
-        step (fn [[total prev-n] c]
-               (let [n (valForChar c)]
-                 [((if (< prev-n n) - +) total prev-n) n]))
+        step       (fn [f c]
+                     (let [n     (valForChar c)       ; Current char's value.
+                           total (f n)                ; We now know how to calc.
+                           ]                          ;  result for PREV values.
+                       #((if (< n %) - +) total n)))  ; Wait! Next value is %.
         ]
-    (first (reduce step [0 0] (str \z roman-str \z)))))
+    ((reduce step identity (str \z roman-str)) 0)))   ; \z inits. total, and
+                                                      ;  0 is for the final %.
+
 
 (deftest solution-test
   (are [result roman-numeral] (= result (solution roman-numeral))
+        1      "I"
+        90     "XC"
         14     "XIV"
         827    "DCCCXXVII"
         3999   "MMMCMXCIX"
